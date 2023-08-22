41719fe9521982af5fcc8dddbec3a3ed8323c5a6
Ankit-Kumar4 <113576375+Ankit-Kumar4@users.noreply.github.com>
2023-08-22 08:15:48 +0530

    Add project files.

---
+++ Program7/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program7
+{
+    internal class Program
+    {
+
+
+        static int SearchValue(int[] array,int target)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] == target)
+                {
+                    return i;
+                }
+
+                return -1;
+            }
+        }
+        static void Main(string[] args)
+        {
+            int[] numbers = { 10, 20, 30, 40, 50 };
+            int targetValue = 30;
+            int index = SearchValue(numbers, targetValue);
+            if (index != -1)
+            {
+                Console.WriteLine($"value{targetValue} found at index{index}");
+            }
+            else
+            {
+                Console.WriteLine($"value{targetValue} not found in the array");
+
+
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
